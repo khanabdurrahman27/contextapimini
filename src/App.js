c9e15da1c4a406d7672adf9a320c5afd38e539de
khanabdurrahman27 <79714622+khanabdurrahman27@users.noreply.github.com>
2024-07-16 01:44:49 +0530

    context api project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import UserContext from './Context/UserContext';
+import UserContextProvider from './Context/UserContextProvider';
+import Login from './Components/Login';
+import Profile from './Components/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContextProvider>
+    <h1>React and chai</h1>
+    <Login/>
+    <Profile/>
+    </UserContextProvider>
   );
 }
 
